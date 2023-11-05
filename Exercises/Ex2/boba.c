49018f9d1d1233ff333d51df2396270d112e2f0e
LordChonk <adrian.hjert@gmail.com>
2023-11-05 20:12:49 +0100

    Done Ex2

---
+++ Exercises/Ex2/boba.c
@@ -0,0 +1,11 @@
+//
+// Created by adrian on 2023-11-05.
+//
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+        printf("Dont't get in my way.\n");
+        return 0;
+}
