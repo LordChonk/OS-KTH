85deab01bdfef5dedcfd938c1d5e115c00af2689
LordChonk <adrian.hjert@gmail.com>
2023-11-07 19:38:13 +0100

    Done Ex3 code

--- Exercises/Ex3/thread2.c
+++ Exercises/Ex3/thread2.c
@@ -5,3 +5,41 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+void *thread_func(void *arg){
+    printf("I am thread #%d\n", *(int *)arg);
+    return NULL;
+}
+/* Extra stuff
+  void *thread_func(void *arg){
+    int code = 5;
+    return (void *)code;
+}
+
+ void *thread_func(void *arg){
+    char buffer[64] = "Hello world!";
+    return buffer;
+}
+
+ void *thread_func(void *arg){
+    char *buffer = (char *)malloc(64);
+    buffer = "Hello world!";
+    return buffer;
+*/
+
+
+int main() {
+    pthread_t t1, t2;
+    int i, j;
+    i = 1;
+    j = 2;
+
+    pthread_create(&t1, NULL, &thread_func, &i);
+    pthread_create(&t2, NULL, &thread_func, &j);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("In main thread\n");
+    return 0;
+}
--- Exercises/Ex3/thread3.c
+++ Exercises/Ex3/thread3.c
@@ -1,3 +1,40 @@
 //
 // Created by adrian on 2023-11-07.
 //
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct thread_args {
+    int a; double b;
+};
+
+struct thread_result{
+    long x; double y;
+};
+
+void *thread_func(void *argsVoid){
+    struct thread_args *args = argsVoid;
+    struct thread_result *res = malloc(sizeof *res);
+
+    res ->x = 10 + args ->a;
+    res ->y = args->a * args->b;
+    return res;
+}
+
+int main(){
+    pthread_t threadL;
+    struct thread_args in = {.a = 10, .b = M_PI};
+    void *out_void;
+    struct thread_result *out;
+
+    pthread_create(&threadL, NULL, thread_func, &in);
+    pthread_join(threadL, &out_void);
+    out = out_void;
+    printf("out -> x = %d\t out -> b = %f\n", out ->x, out ->y);
+    free(out);
+
+    return 0;
+}
